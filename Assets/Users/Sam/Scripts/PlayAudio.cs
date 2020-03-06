f692bb390b3f3d7669a5d79de62b5a44c2512711
S-fisk <fisks51@gmail,com>
2020-03-06 03:12:48 +0000

    Audio_Test
    Added the background music and bird sounds. For now the piano track is looped and the bird sounds play once. However, the bird sounds can be stacked and sound good overlapped.

---
+++ Assets/Users/Sam/Scripts/PlayAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource Sound;
+    public bool alreadyPlayed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter()
+    {
+        if (!alreadyPlayed) 
+        {
+            Sound.PlayOneShot(SoundToPlay, Volume);
+            alreadyPlayed = true;
+        }
+    }
+
+}
