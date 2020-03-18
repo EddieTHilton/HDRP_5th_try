97e1e9cb3436036eb65c116c8bba37ab7d6bf302
Joao Mondego <jpmondego1999@live.com.pt>
2020-03-18 16:19:58 +0000

    Menu
    Nearly completed menu

--- Assets/Users/Joao/Scripts/MenuScript.cs
+++ Assets/Users/Joao/Scripts/MenuScript.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    public GameObject SceneToLoad;
+    public string SceneToLoad;
 
-    
+    public void SceneLoader()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+        Debug.Log("Loaded" + SceneToLoad);
+    }
 
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("QuitGame");
+    }
 }
