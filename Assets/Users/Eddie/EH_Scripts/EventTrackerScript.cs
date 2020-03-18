20de9d9bafa5076816813dad5d4df0da544844be
EddieTHilton <58077810+EddieTHilton@users.noreply.github.com>
2020-03-18 21:16:54 +0000

    Minor Event Tracking fixed

--- Assets/Users/Eddie/EH_Scripts/EventTrackerScript.cs
+++ Assets/Users/Eddie/EH_Scripts/EventTrackerScript.cs
@@ -8,17 +8,23 @@ public class EventTrackerScript : MonoBehaviour
     public int majorEventsTotal = 0;
     public int majorEventsActive = 0;
 
+    public int minorEventsTotal = 0;
+    public int minorEventsActive = 0;
+
     private GameObject MajorTracker;
+    private GameObject MinorTracker;
 
     private void Start()
     {
         MajorTracker = GameObject.Find("Major Events");
+        MinorTracker = GameObject.Find("Minor Events");
 
     }
 
     private void Update()
     {
         MajorTracker.GetComponent<Text>().text = ("Major Events: \n" + majorEventsActive + " / " + majorEventsTotal);
+        MinorTracker.GetComponent<Text>().text = ("Minor Events: \n" + minorEventsActive + " / " + minorEventsTotal);
     }
 
 }
--- Assets/Users/Eddie/EH_Scripts/Grove_Prototype_Scripts/EH_ActivateOnInteract.cs
+++ Assets/Users/Eddie/EH_Scripts/Grove_Prototype_Scripts/EH_ActivateOnInteract.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class EH_ActivateOnInteract : MonoBehaviour
 {
     public GameObject c_thischild;
-
+    private GameObject EventTracker;
+    private bool completed = false;
 
     public void Start()
     {
         //Assigns the transform of the first child of the Game Object this script is attached to.
         c_thischild = this.gameObject.transform.GetChild(0).gameObject;
+
+
+        EventTracker = GameObject.Find("Event Tracker");
+        EventTracker.GetComponent<EventTrackerScript>().minorEventsTotal++;
     }
 
     public void OnTriggerStay(Collider collision)
     {
-        if (collision.gameObject.tag == "PlayerReach" && Input.GetKey(KeyCode.E)) //PlayerReach, is the range within which the player-character can touch surrounding objects
+        if (collision.gameObject.tag == "PlayerReach" && Input.GetKey(KeyCode.E) && completed == false) //PlayerReach, is the range within which the player-character can touch surrounding objects
         {
             c_thischild.SetActive(true);
+
+            EventTracker.GetComponent<EventTrackerScript>().minorEventsActive++;
+            completed = true;
         }
     }
 }
